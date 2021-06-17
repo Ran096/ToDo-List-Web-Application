4ed1b3bdf404217c816044b6fe59513006dee0bd
Ran096 <54087886+Ran096@users.noreply.github.com>
2021-06-17 10:40:58 +0530

    Todo List application Create
    Todo List application Create

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import ToDolists from './ToDolists';
+
+function App() {
+  const [inputList, setInputList] =useState("");
+  const [Items,setItems] = useState([]); 
+  const itemEvent =(event)=>{
+    setInputList(event.target.value);
+  }
+  const listOfItems =()=>{
+     setItems((oldItems)=>{
+       return [...oldItems, inputList];
+     });
+     setInputList("");
+  };
+  const deleteItems=(id)=>{
+    console.log('DeleteItems ');
+    setItems ((oldItems)=>{
+     return oldItems.filter((arrElem,index)=>{
+       return index!==id ;
+     })
+    }
+    )
+  
+ };
+
+  return (
+    <div className="Main_div">
+      <div className="center_div">
+        <br/>
+        <h1> ToDo List</h1>
+        <br/>
+        <input type="text" placeholder="Add a items"value={inputList} onChange={itemEvent}/>
+        <button onClick={listOfItems}> + </button>
+
+        <ol>
+         { Items.map( (itemval,index) => {  
+           return <ToDolists
+            key={index}
+            id={index}
+            text={itemval}
+            onSelect={deleteItems}/>
+           
+          } )}
+        </ol>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
